4c3cdbdbc51ee62a896e00beb0301999a3762707
Ulises Herzel <pherzel@intouchhealth.com>
2023-01-20 12:19:35 -0300

    Refactor Socials Media and add TOS

---
+++ src/data/socials.ts
@@ -0,0 +1,22 @@
+export const socials = [
+  {
+    title: "Instagram",
+    icon: "mdi:instagram",
+    url: "https://www.instagram.com/navrajchohan44",
+  },
+  {
+    title: "Twitter",
+    icon: "mdi:twitter",
+    url: "https://twitter.com/Up_Mortem/",
+  },
+  {
+    title: "LinkedIn",
+    icon: "mdi:linkedin",
+    url: "https://www.linkedin.com/in/navraj-chohan-phd-84a61b26/",
+  },
+  {
+    title: "Reddit",
+    icon: "mdi:reddit",
+    url: "https://www.reddit.com/r/upmortem/",
+  },
+];
